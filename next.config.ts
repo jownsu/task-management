535713ce7a7a697d11d5a7d2a8fe08388a894e65
Jownsu <jhonesdigno666@gmail.com>
2025-04-12 16:41:32 +0800

    Setup svgr

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+  
   /* config options here */
   images: {
     remotePatterns: [
