21e6d1b721fdba515726870ddd2d4196484efc0d
Jownsu <jhonesdigno666@gmail.com>
2025-04-19 17:06:28 +0800

    Fix build error

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,7 +3,7 @@ import SocialsLogin from "@/components/auth/socials-login";
 
 const LoginPage = () => {
 	return (
-		<div className="w-full flex items-center justify-center min-h-screen">
+		<div className="w-full flex items-center justify-center min-h-screen px-[16]">
 			<SocialsLogin />
 		</div>
 	);
