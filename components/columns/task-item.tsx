e11d711018f46f3bdfea96ce810f4426b742a1fd
Jownsu <jhonesdigno666@gmail.com>
2025-05-10 14:25:24 +0800

    Added hover

--- components/columns/task-item.tsx
+++ components/columns/task-item.tsx
@@ -1,3 +1,4 @@
+/* CONSTANTS */
 import { Task } from "@/constants/types";
 
 interface Props {
@@ -7,10 +8,12 @@ interface Props {
 const TaskItem = ({ task }: Props) => {
 	return (
 		<button
-			className="bg-foreground rounded-lg px-[16] py-[24] text-left flex flex-col gap-[8] drop-shadow-md"
+			className="bg-foreground rounded-lg px-[16] py-[24] text-left flex flex-col gap-[8] drop-shadow-md cursor-pointer group"
 			type="button"
 		>
-			<p className="text-h-md text-black dark:text-white">{task.title}</p>
+			<p className="text-h-md text-black dark:text-white group-hover:text-primary dark:group-hover:text-primary">
+				{task.title}
+			</p>
 			<p className="text-b-md text-medium-grey">0 of 3 subtasks</p>
 		</button>
 	);
