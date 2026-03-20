b6345f9da8cfeef2777bb485ff66bf8e2b45970f
Jhones <jhonesdigno666@gmail.com>
2026-03-20 21:50:54 +0800

    feat: configure Prisma adapter for SSL in production environment

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,7 +3,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@/lib/generated/prisma/client";
 import { ENV } from "@/constants/env";
 
-const adapter = new PrismaPg({ connectionString: ENV.DATABASE_URL });
+const adapter = new PrismaPg({ connectionString: ENV.DATABASE_URL, ssl: ENV.NODE_ENV === "production" ? { rejectUnauthorized: true } : false });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
